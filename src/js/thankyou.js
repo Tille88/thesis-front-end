6e877cdd038b109a985b4ccc9110cf62d3505f4f
Jonas Tillman <tillman.jonas@gmail.com>
2021-01-04 11:44:03 +0800

    Celebratory thank-you page

--- src/js/thankyou.js
+++ src/js/thankyou.js
@@ -1,1 +1,19 @@
 import "../style/page-thank-you.scss";
+const confetti = require('canvas-confetti');
+
+var myCanvas = document.createElement('canvas');
+document.body.appendChild(myCanvas);
+
+var myConfetti = confetti.create(myCanvas, {
+  resize: true
+});
+
+myConfetti({
+    particleCount: 100,
+    startVelocity: 30,
+    spread: 360,
+    origin: {
+      x: 0.5,
+      y: 0.4
+    }
+  });
